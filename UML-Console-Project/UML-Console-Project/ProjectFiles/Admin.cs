7e2401b2e1a763fb58b577cadaca02881e5f5907
JEBREEL MAJDALWIEH <jemajdalwieh18@cit.just.edu.jo>
2021-05-21 05:43:17 +0300

    Add project files.

---
+++ UML-Console-Project/UML-Console-Project/ProjectFiles/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Console_Project.ProjectFiles
+{
+    class Admin
+    {
+        string Name;
+
+        public void Additem()
+        {
+
+        }
+    }
+}
