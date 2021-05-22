3f1a311382928249c400ef977780dff1f084236d
ISLAM ALSHQEIRAT <ezalshuqirat18@cit.just.edu.jo>
2021-05-22 01:57:27 -0700

    update

--- UML-Console-Project/UML-Console-Project/ProjectFiles/Provider.cs
+++ UML-Console-Project/UML-Console-Project/ProjectFiles/Provider.cs
@@ -10,12 +10,7 @@ namespace UML_Console_Project.ProjectFiles
 {
     class Provider
     {
-        //hi islam
-        //this is shatha's work she was trying to understand some concept 
-        //feel free to delete it or change it 
-        //but try to get shatha updated about the way you wanna change it or rewrite it
-        //<jebril>
-
+      
         private string Name;
         private string Category;
         private string Location;
@@ -47,6 +42,22 @@ namespace UML_Console_Project.ProjectFiles
                 t++;
             }
         }
+
+        public void InaitialData()
+        { 
+            Item [] i0= new item[4];
+            i0[0]=
+            MySystem.p[0] ={"Hot and Cold","Food","Area1",0.68,0.05, ,100}
+            MySystem.p[1] ={"MY Bread","Food","Area1",0.75,0.07, ,200}
+            MySystem.p[2] ={"Good Recipe","Food","Area2",0.9,0.12, ,300}
+            MySystem.p[3] ={"Tasty","Food","Area3",0.73,0.06, ,400}
+            MySystem.p[4] ={"Stop Here","Market","Area1",0.88,0.1, ,500}
+            MySystem.p[5] ={"Good Mart","Market","Area2",0.95,0.15, ,600}
+            MySystem.p[6] ={"WMs","Market","Area2",0.82,0.09, ,700}
+            MySystem.p[7] ={"24Hours","Market","Area3",0.79,0.08, ,800}
+            MySystem.p[8] ={"C-Market","Market","Area1",0.92,0.14, ,900}
+        }
+        
 
         /*void AddInitialData()
         {
